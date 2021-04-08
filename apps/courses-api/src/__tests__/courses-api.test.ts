9e779c2c5e78cb3e23a011f3ed3818ab0f21b6f2
ywarezk <ywarezk@gmail.com>
2021-04-08 22:11:00 +0300

    courses-api: running the first test

---
+++ apps/courses-api/src/__tests__/courses-api.test.ts
@@ -0,0 +1,17 @@
+/**
+ * Testing the courses api
+ *
+ * Created April 8th, 2021
+ * @author: ywarezk
+ * @version: 0.0.1
+ * @license: MIT
+ */
+
+import { beforeAfterBootstrap } from '@nz/test/express';
+import { expect } from 'chai';
+
+describe('courses api', () => {
+  it('sanity', () => {
+    expect(true).to.equal(true);
+  })
+})
