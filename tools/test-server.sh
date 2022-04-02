78f6f1ec9b2b9bbaee8c3268ed1077057b4fb211
ywarezk <ywarezk@gmail.com>
2022-04-02 16:11:40 +0300

    lessons: the test command is now passing, working on the deploy command

--- tools/test-server.sh
+++ tools/test-server.sh
@@ -9,12 +9,10 @@
 # @copyright: yellowHEAD
 #
 
-cd apps/$1
-export COVERAGE_DIR=../../../reports/$1
-export JUNIT_REPORT_PATH=../../../reports/$1/unit-test.xml
-npx jenkins-mocha --cobertura --reporter mocha-jenkins-reporter --timeout 1200000 -r ../../../node_modules/ts-node/register -r ../../../node_modules/tsconfig-paths/register **/*.spec.ts
+export COVERAGE_DIR=reports/$1
+export JUNIT_REPORT_PATH=reports/$1/unit-test.xml
+npx jenkins-mocha --cobertura --reporter mocha-jenkins-reporter --timeout 1200000 -r ts-node/register -r tsconfig-paths/register $1/**/*.spec.ts
 status=$?
-cd ../../..
 mv reports/$1/cobertura-coverage.xml reports/$1/coverage.xml
 exit $status
 
