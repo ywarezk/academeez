fd8781e6eed8c9ad7acb8113ea5b981b5111ecd3
ywarezk <ywarezk@gmail.com>
2022-06-04 20:12:23 +0300

    az: card with long title will display a tooltip

--- apps/az/home/HomePage/CardLesson/CardLesson.spec.tsx
+++ apps/az/home/HomePage/CardLesson/CardLesson.spec.tsx
@@ -7,27 +7,30 @@
  * @license: MIT
  */
 
-import { render } from '@testing-library/react';
-import { CardLesson } from './CardLesson';
-import { ThemeProvider } from '@az/styles';
+import { render } from '@testing-library/react'
+import { CardLesson } from './CardLesson'
+import { ThemeProvider } from '@az/styles'
 
 describe('<CardLesson />', () => {
-  it('Sanity', function() {
+  it('Sanity', function () {
     render(
       <ThemeProvider>
-
-        <CardLesson lesson={{
-          title: 'React Introduction',
-          description: `
+        <CardLesson
+          lesson={{
+            title: 'React Introduction',
+            description: `
             In this chpater we will go over the basics of React.
             What can we create with react, about the DOM, and why choose React.
             Entry level lesson to learn react
           `,
-          bgImage: 'https://github.com/ywarezk/academeez/raw/main/libs/courses/05_angular/bg.png',
-          duration: 66,
-          link: '/courses/react/introduction',
-          videoUrl: 'https://c563a7c556a91cdae32025bd25037548.egress.mediapackage-vod.eu-west-1.amazonaws.com/out/v1/c981d287b93543028c69b53d164248a3/9066c336968343c59ae6119f795c7d7c/9a1daa110f12415ebc81ad4bb65d7e95/index.m3u8'
-        }} />
+            bgImg:
+              'https://github.com/ywarezk/academeez/raw/main/libs/courses/05_angular/bg.png',
+            duration: 66,
+            link: '/courses/react/introduction',
+            videoUrl:
+              'https://c563a7c556a91cdae32025bd25037548.egress.mediapackage-vod.eu-west-1.amazonaws.com/out/v1/c981d287b93543028c69b53d164248a3/9066c336968343c59ae6119f795c7d7c/9a1daa110f12415ebc81ad4bb65d7e95/index.m3u8',
+          }}
+        />
       </ThemeProvider>
     )
   })
