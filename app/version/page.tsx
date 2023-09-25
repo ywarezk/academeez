ba164fa9ead85bc691f863fcdf60c1318ad32fa4
Yariv Katz <ywarezk@gmail.com>
2023-09-25 17:11:51 +0300

    feat: added a version page

---
+++ app/version/page.tsx
@@ -0,0 +1,12 @@
+/**
+ * Just printing the current version
+ *
+ * Created September 25th, 2023
+ * @author ywarezk
+ * @version 0.0.2
+ * @license MIT
+ */
+
+export default function Version() {
+  return <p>v0.0.2</p>
+}
