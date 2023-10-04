9a4a2c546425c1aef2c2e1d8f331f29bcac1608e
Yariv Katz <ywarezk@gmail.com>
2023-10-04 16:29:17 +0300

    New translations toc.ts (Hebrew) [ci skip]

---
+++ content/he/course/angular/toc.ts
@@ -0,0 +1,25 @@
+/**
+ * Table of content for React course
+ *
+ * Created September 13th, 2023
+ * @author ywarezk
+ * @version 0.0.1
+ * @license MIT
+ */
+import { NavItem } from '@/lib';
+import { AngularIcon } from '@/ui/icons';
+const toc: NavItem = {
+  title: 'Angular',
+  href: '/en/course/angular',
+  Icon: AngularIcon,
+  items: [{
+    title: 'NGRX',
+    href: '/en/course/angular/ngrx',
+    items: [{
+      title: 'Installing NGRX',
+      href: '/en/course/angular/ngrx/installation',
+      items: []
+    }]
+  }]
+};
+export default toc;
