50a30032d2b2322ae3636675c560f8e1b3cd6815
Yariv Katz <ywarezk@gmail.com>
2023-10-07 08:13:29 +0300

    feat: added code block to mdx

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -78,6 +78,7 @@ export const Doc = defineDocumentType(() => ({
 export default makeSource({
   contentDirPath: './content',
   documentTypes: [Doc],
+  contentDirExclude: ['**/*.md'],
   mdx: {
     remarkPlugins: [remarkGfm, codeImport],
     rehypePlugins: [
