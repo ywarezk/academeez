5652466bc533bba80056250dcc182a4e7c1c311d
Yariv Katz <ywarezk@gmail.com>
2024-08-27 15:51:20 +0300

    initial

---
+++ astro.config.mjs
@@ -0,0 +1,61 @@
+import {defineConfig} from 'astro/config';
+import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    react({
+      experimentalReactChildren: true,
+    }),
+    starlight({
+      editLink: {
+        baseUrl: 'https://github.com/ywarezk/academeez/edit/main/docs/'  
+      },
+      components: {
+        Sidebar: './src/components/Sidebar.astro',  
+        PageSidebar: './src/components/PageSidebar.astro',
+      },
+      sidebar: [
+        {  
+          label: 'Angular',        
+          autogenerate: {directory: 'courses/angular', collapsed: true},
+        },
+        {  
+          label: 'Kubernetes',
+          autogenerate: {directory: 'courses/kubernetes', collapsed: true},
+        },
+      ],
+      title: 'academeez',
+      logo: {
+        light: './src/assets/logo-line.svg',
+        dark: './src/assets/logo-line.svg',
+        replacesTitle: true,
+      },
+      defaultLocale: 'root',
+      locales: {
+        root: {
+          label: 'English',
+          lang: 'en',
+        },
+        he: {
+          label: 'עברית',
+          dir: 'rtl',
+          lang: 'he',
+        },
+      },
+      social: {
+        github: 'https://github.com/ywarezk/academeez',
+        youtube: 'https://www.youtube.com/channel/UCmnTSM4hGDJin7g5PyXa9pQ',
+      },
+      customCss: ['./src/tailwind.css'],
+    }),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  prefetch: true,
+  site: 'https://www.academeez.com',
+  trailingSlash: 'never',
+});
