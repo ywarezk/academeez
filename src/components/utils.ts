43717f987f0f11563c04c95f9ce05f783fbaf2d6
Yariv Katz <ywarezk@gmail.com>
2024-09-08 16:07:48 +0300

    fix: locale issue on sidebar breadcrumbs

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -6,3 +6,7 @@ import {twMerge} from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function isRtl(locale: string) {
+  return ['ar', 'he'].includes(locale)
+}
