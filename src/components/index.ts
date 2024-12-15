54915fb36de8fc82bad8fc533fda2a487bdfac8c
Yariv Katz <ywarezk@gmail.com>
2024-12-15 17:19:36 +0200

    fix: astro:content cannot be imported from barrel file cause it will cause issues with client rendered components

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,6 +4,5 @@ export { Button } from './Button';
 export * from './Card';
 export * from './LessonCard';
 export * from './Carousel';
-export { default as LocalCourseCarousel} from './LocaleCourseCarousel.astro';
 export * from './Breadcrumb';
 export * from './Avatar';
