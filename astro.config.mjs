13d971af51345807f4210220705e611bb4df3a1a
Yariv Katz <ywarezk@gmail.com>
2024-12-18 12:38:21 +0200

    debug: sitemap bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,21 +10,29 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react({
+  integrations: [sitemap({
+    i18n: {
+      defaultLocale: 'root',
+      locales: {
+        root: 'en-US',
+        he: 'he-IL'
+      }
+    }
+  }), react({
     experimentalReactChildren: true
   }), starlight({
     head: [{
       tag: 'meta',
       attrs: {
         name: "google-site-verification",
-        content: "ZLdedo-l5mm1TcXoJ4Ikyasbj8wnH4iHMyMuWu2do9s"        
+        content: "ZLdedo-l5mm1TcXoJ4Ikyasbj8wnH4iHMyMuWu2do9s"
       }
     }, {
       tag: 'meta',
       attrs: {
         name: "msvalidate.01",
         content: "9EEEA5142FA10331B8194AC8059955E9"
-      }      
+      }
     }],
     favicon: '/favicon-32x32.png',
     editLink: {
@@ -36,8 +44,8 @@ export default defineConfig({
       SocialIcons: './src/components/SocialIcons.astro',
       PageTitle: './src/components/PageTitle.astro',
       ThemeSelect: './src/components/ThemeSelect.astro',
-      MarkdownContent: './src/components/MarkdownContent.astro',      
-      Pagination: './src/components/Pagination.astro',      
+      MarkdownContent: './src/components/MarkdownContent.astro',
+      Pagination: './src/components/Pagination.astro',
     },
     sidebar: [{
       label: 'Angular',
@@ -101,22 +109,14 @@ export default defineConfig({
     customCss: ['./src/tailwind.css']
   }), tailwind({
     applyBaseStyles: false
-  }), sitemap({
-    i18n: {
-      defaultLocale: 'root',
-      locales: {
-        root: 'en-US',
-        he: 'he-IL'
-      }
-    }
   }), icon()],
   markdown: {
-    remarkPlugins: [mermaid],    
+    remarkPlugins: [mermaid],
   },
   prefetch: true,
   site: 'https://www.academeez.com',
   trailingSlash: 'never',
-  output: "server",  
+  output: "server",
   adapter: vercel({
     webAnalytics: {
       enabled: true,
