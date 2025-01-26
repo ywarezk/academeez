e0ae58021d25501b7f8814eaf2c72973ee4d8d6f
Yariv Katz <ywarezk@gmail.com>
2025-01-26 19:43:04 +0200

    feat: installed prettier and eslint

--- src/plugins/mermaid.ts
+++ src/plugins/mermaid.ts
@@ -1,26 +1,26 @@
-import type { RemarkPlugin } from "@astrojs/markdown-remark"
-import { visit } from "unist-util-visit"
+import type { RemarkPlugin } from '@astrojs/markdown-remark';
+import { visit } from 'unist-util-visit';
 
 const escapeMap: Record<string, string> = {
-  "&": "&amp;",
-  "<": "&lt;",
-  ">": "&gt;",
-  '"': "&quot;",
-  "'": "&#39;",
-}
+	'&': '&amp;',
+	'<': '&lt;',
+	'>': '&gt;',
+	'"': '&quot;',
+	"'": '&#39;',
+};
 
-const escapeHtml = (str: string) => str.replace(/[&<>"']/g, c => escapeMap[c])
+const escapeHtml = (str: string) => str.replace(/[&<>"']/g, (c) => escapeMap[c]);
 
-export const mermaid: RemarkPlugin<[]> = () => tree => {	
-  visit(tree, "code", node => {
-    if (node.lang !== "mermaid") return
+export const mermaid: RemarkPlugin<[]> = () => (tree) => {
+	visit(tree, 'code', (node) => {
+		if (node.lang !== 'mermaid') return;
 
-    // @ts-ignore
-    node.type = "html"
-    node.value = `
+		// @ts-expect-error -- node type is not defined in unist
+		node.type = 'html';
+		node.value = `
       <div class="mermaid" data-content="${escapeHtml(node.value)}">
         <p>Loading graph...</p>
       </div>
-    `
-  })
-}
+    `;
+	});
+};
