e1f111147ce79bcd5c274125cb4bb02636662674
Yariv Katz <ywarezk@gmail.com>
2025-08-19 17:25:18 +0300

    feat(ci): upload playwright trace (#17)
    * feat: configured playwright to record video and snapshot on fail
    
    * feat(ci): collecting playwright artifacts

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
 	forbidOnly: !!process.env.CI,
 	/* Retry on CI only */
-	retries: process.env.CI ? 2 : 0,
+	retries: process.env.CI ? 3 : 0,
 	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
@@ -30,6 +30,8 @@ export default defineConfig({
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry',
+		video: 'retain-on-failure',
+		screenshot: 'only-on-failure',
 	},
 
 	/* Configure projects for major browsers */
