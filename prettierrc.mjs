aa4968b27fd814417a9f09ca21a5dd3a26201f80
Yariv Katz <ywarezk@gmail.com>
2025-08-22 14:22:19 +0300

    feat: prettier configurations

--- prettierrc.mjs
+++ prettierrc.mjs
@@ -1,12 +1,18 @@
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro"],
-  overrides: [
-    {
-      files: "*.astro",
-      options: {
-        parser: "astro",
-      },
-    },
-  ],
+	printWidth: 120,
+	semi: true,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'es5',
+	useTabs: false,
+	plugins: ['prettier-plugin-astro'],
+	overrides: [
+		{
+			files: '*.astro',
+			options: {
+				parser: 'astro',
+			},
+		},
+	],
 };
