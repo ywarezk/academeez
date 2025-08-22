9137093ee336982a5aa9f3790c12953912e129b5
Yariv Katz <ywarezk@gmail.com>
2025-08-22 14:55:24 +0300

    feat(ci): eslint, prettier, astro check (#18)
    * feat: eslint configurations
    
    * fix: eslint errors
    
    * feat: prettier configurations
    
    * feat: vscode settings.json
    
    if developers complain about this i will remove this
    and set to settings.example.json
    
    * fix: prettier
    
    * feat(ci): added check workflow
    
    this will run eslint, prettier, astro check

--- src/integrations/sitemap-copier.ts
+++ src/integrations/sitemap-copier.ts
@@ -1,32 +1,32 @@
-import type { AstroIntegration } from "astro";
-import { readdir, cp } from "node:fs/promises";
-import * as path from "node:path";
+import type { AstroIntegration } from 'astro';
+import { readdir, cp } from 'node:fs/promises';
+import * as path from 'node:path';
 
 export function sitemapCopier(): AstroIntegration {
 	return {
-		name: "sitemap-copier",
+		name: 'sitemap-copier',
 		hooks: {
-			"astro:build:done": async ({ logger }) => {
-				const buildLogger = logger.fork("sitemap-copier");
-				buildLogger.info("Copying xml files from dist to vercel out");
+			'astro:build:done': async ({ logger }) => {
+				const buildLogger = logger.fork('sitemap-copier');
+				buildLogger.info('Copying xml files from dist to vercel out');
 				try {
-					const files = await readdir("./dist/client");
+					const files = await readdir('./dist/client');
 					const xmlFiles = files.filter(
 						(file) =>
-							path.extname(file).toLowerCase() === ".xml" &&
-							path.basename(file).toLowerCase().startsWith("sitemap")
+							path.extname(file).toLowerCase() === '.xml' &&
+							path.basename(file).toLowerCase().startsWith('sitemap')
 					);
-					buildLogger.info(xmlFiles.join(", "));
+					buildLogger.info(xmlFiles.join(', '));
 					for (const file of xmlFiles) {
-						const sourcePath = path.join("./dist/client", file);
-						const destPath = path.join("./.vercel/output/static", file);
+						const sourcePath = path.join('./dist/client', file);
+						const destPath = path.join('./.vercel/output/static', file);
 						await cp(sourcePath, destPath);
 					}
-					buildLogger.info("All XML files copied successfully");
+					buildLogger.info('All XML files copied successfully');
 				} catch (error) {
 					buildLogger.error(`Error copying files: ${error}`);
 				}
-			}
-		}
+			},
+		},
 	};
 }
